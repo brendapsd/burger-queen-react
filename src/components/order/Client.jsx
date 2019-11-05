6b1a42085572373c9ea7c0c5b1f9b9b2870e8d89
brendapsd <brenda.sd.15@gmail.com>
2019-11-05 17:11:25 -0500

    se instalo firebase, maquetado de componente header y client

---
+++ src/components/order/Client.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Client() {
+    return (
+      <div className="Client">
+        <input type="text" placeholder="Ingresa nombre del cliente"/>
+      </div>
+    );
+  }
+  
+  export default Client;
